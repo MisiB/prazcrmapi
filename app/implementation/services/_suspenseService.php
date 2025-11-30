12696dee3e0fa0010d23454f4319ad9b222d7e24
MisiB <misib@praz.org.zw>
2025-11-30 22:04:10 +0200

    new changes

--- app/implementation/services/_suspenseService.php
+++ app/implementation/services/_suspenseService.php
@@ -82,7 +82,7 @@ class _suspenseService implements isuspenseService
         $invoiceAmount = (float) str_replace(',', '', $invoice->amount);
 
         if($invoiceAmount > $walletbalance){
-            return ['status'=>'ERROR','message'=>"Insufficient funds in wallet of type ".$data['accounttype']." using currency ".$invoice->currency->name];
+            return ['status'=>'ERROR','message'=>"Insufficient funds in wallet of type ".$data['accounttype']." using currency ".$invoice->currency->name."invoice amount: ".$invoiceAmount." wallet balance: ".$walletbalance];
         }
         
         //get pendingsuspense - we NEED these records to exist!
